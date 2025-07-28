b34acbe7321bacb7c7f7d74228ae0740a1df911a
Birk Tjelmeland <git@birktj.no>
2025-07-28 17:28:51 +0200

    Add simple nixos image

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.05";
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+    
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = { self, nixpkgs, flake-compat }@inputs:
+  outputs = { self, nixpkgs, flake-compat, nixos-hardware }@inputs:
     let
       systems = nixpkgs.lib.platforms.linux;
       lib = nixpkgs.lib;
@@ -13,5 +15,97 @@
       legacyPackages = packages;
       overlay = final: prev: (lib.mapAttrs (n: v: prev.callPackage v { }) packagePaths);
       nixosModules = { linuxcnc = import ./modules/linuxcnc.nix; };
+
+
+      nixosConfigurations.flap-display-controller = nixpkgs.lib.nixosSystem {
+            modules = [
+                "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+                "${nixos-hardware}/raspberry-pi/4/pkgs-overlays.nix"
+                self.nixosModules.linuxcnc
+                ({pkgs, ...}: {
+                    nixpkgs.overlays = [
+                        self.overlay
+                        (final: prev: {
+                            python312 = prev.python312.override {
+                                packageOverrides = pyself: pysuper: {
+                                    psutil = pysuper.psutil.overrideAttrs (_: {
+                                        # doCheck = false;
+                                        # doInstallCheck = false;
+                                    });
+                                };
+                            };
+                        })
+                    ];
+
+                    # nixpkgs.buildPlatform = "x86_64-linux";
+
+                    local.packages.linuxcnc.enable = true;
+                
+                    # Setup nix flakes
+                    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+                    nix.registry = {
+                        nixpkgs.to = {
+                            type = "path";
+                            path = pkgs.path;
+                        };
+                    };
+                    nix.settings.trusted-users = ["@wheel"];
+                    
+                    security.sudo = {
+                        enable = true;
+                        wheelNeedsPassword = false;
+                    };
+                    users.mutableUsers = false;
+
+                    users.users.birk = {
+                        isNormalUser = true;
+                        extraGroups = [ "wheel" "dialout" ]; # Enable ‘sudo’ for the user.
+                        openssh.authorizedKeys.keys = [ 
+                            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrvdnfRte2aM39d+GdUVt+KI6HqP8opmmuxYXKdBMzF birk@erebus"
+                            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuZp5Quo2ubCkxqX6D1DIqKVf+p98ffcNg6f9M6Nc9X birk@granite"
+                        ];
+                    };
+
+                    # Setup openssh
+                    services.openssh.enable = true;
+                    networking.firewall.allowedTCPPorts = [
+                        22  
+                    ];
+
+                    environment.systemPackages = with pkgs; [
+                        vim
+                        helix
+                        picocom
+                        i2c-tools
+                        dtc
+                        flap-display-controller
+                    ];
+
+                    # Setup network
+                    networking.wireless = {
+                        enable = true;
+                        networks.ArkytaNett.psk = "froskspikerpistolvannkanne";
+                    };
+                    networking.hostName = "flap-display-controller";
+
+                    # Setup RTC
+                    hardware.raspberry-pi."4".apply-overlays-dtmerge.enable = true;
+                    hardware.deviceTree = {
+                        enable = true;
+                        overlays = [
+                            { name = "i2c-rtc"; dtsFile = ./nix/rtc/i2c-rtc.dts; }
+                        ];
+                    };
+                    
+                    nixpkgs.hostPlatform.system = "aarch64-linux";
+                    # nixpkgs.buildPlatform.system = "x86_64-linux";
+                    system.stateVersion = "23.11";
+
+                    sdImage.compressImage = false;
+                })
+            ];
+       };
+
+        images.flap-display-controller = self.nixosConfigurations.flap-display-controller.config.system.build.sdImage;
     };
 }
